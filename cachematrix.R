eac70f77c4901c5b7ae63af50eabf0bd460554a8
David A Spitzley <dspitzle@washtenawisd.org>
2015-09-16 14:51:33 -0400

    Define functions
    Fleshes out the makeCacheMatrix and cacheSolve function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions enable caching of 
+## matrix inversions to minimize processor load
 
-## Write a short comment describing this function
+## makeCacheMatrix():  Creates a list of functions
+##                     for manipulating and inverting matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve():  Inverts a matrix x, unless the inverse
+##                has been cached, in which case the cache is returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv		
+		
 }
